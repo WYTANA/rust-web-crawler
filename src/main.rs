2362bfa44191bf16dfc3a3328b94b42b80056e84
Travis L Orback <tlorback@gmail.com>
2022-08-02 00:33:43 -0600

    run file and add libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use html5ever::tokenizer::{
+    BufferQueue,
+    Tag,
+    TagKind,
+    Token,
+    TokenSink,
+    TokenSinkResult,
+    Tokenizer,
+    TokenizerOpts, // TagToken
+};
+use std::borrow::Borrow;
+use url::{ParseError, Url};
+
+#[derive(Debug, Default)]
+struct LinkQueue {
+    links: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
 }
